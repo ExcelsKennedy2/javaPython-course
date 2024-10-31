28c68755edd983e40aeb0893f057be0e01bffb10
ExcelsKennedy2 <kennedymbugua841@gmail.com>
2024-10-31 15:09:53 +0300

    edited files

--- assignment.py
+++ assignment.py
@@ -76,12 +76,15 @@ def fibonnaci2(n):
         return []
     elif n == 1:
         return [0]
+    elif n == 2:
+        return [0,1]
 
     fib_sequence = [0, 1]
     for i in range(2, n):
-        # fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
-        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
-        return  fib_sequence
+        fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
+        # fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+
+    return  fib_sequence
 
 print(fibonnaci2(5))
 print(fibonnaci2(10))
